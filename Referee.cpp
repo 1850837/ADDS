2d67f2f4b2f43d7983812de8eeb565a84ffd8269
1850837 <sevensixthree763@gmail.com>
2025-03-14 17:05:54 +1030

    debugging

--- Referee.cpp
+++ Referee.cpp
@@ -9,11 +9,16 @@ Player* Referee::refGame(Player* player1, Player* player2){
     //variable declaration
     //There is a loop to check if player 1 won or if there is a tie
     //As such, the player ptr should be set to player2
-    Player* ptr = player2;
+    // Player* ptr = player2;
 
     //set the moves
     player1->makeMove();
     player2->makeMove();
+
+    //checking for a tie by comparing the names of the moves chosen
+    if(player1->getMove()->getName() == player2->getMove()->getName()){
+        return nullptr;
+    }
 
     //if-statement for determining which is stronger
     //nested in a for-loop to iterate through the length of the vector
@@ -24,15 +29,12 @@ Player* Referee::refGame(Player* player1, Player* player2){
         //and if so, sets player1 as the winner
         //if not, player2 is in ptr
         if(player2->getMove()->getName() == player1->getMove()->getVector()[i]){
-            ptr = player1;      //changes the ptr variable to the winning person
+            return player1;      //changes the ptr variable to the winning person
         }
     }
 
-    //checking for a tie by comparing the names of the moves chosen
-    if(player1->getMove()->getName() == player2->getMove()->getName()){
-        ptr = nullptr;
-    }
 
-    return ptr;
+
+    return player2;
 
 };
